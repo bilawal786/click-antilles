cf324e904febeb65177439c3192c07fd4ebb3fbe
mughalusman <mughalusman5554@gmail.com>
2022-10-20 18:42:26 +0500

    okay

--- app/Http/Controllers/Website/FrontController.php
+++ app/Http/Controllers/Website/FrontController.php
@@ -88,7 +88,7 @@ class FrontController extends Controller
     {
         if ($id != null && $position != null) {
 
-            $products = Product::where('featured', 1)->whereJsonContains('category_ids', [
+            $products = Product::query()->where('featured', 1)->whereJsonContains('category_ids', [
                 ['id' => strval($id), 'position' => (int)$position],
                 ])
                 ->paginate(10);
